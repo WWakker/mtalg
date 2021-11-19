7d2e1b0c18a3441dc410870660e8d4ebc68f4589
Wakker, Wouter <Wouter.Wakker@ecb.europa.eu>
2021-11-19 09:37:49 +0100

    Renamed threads, added shape check

--- mtalg/random/random_number_generators.py
+++ mtalg/random/random_number_generators.py
@@ -3,22 +3,23 @@ import multiprocessing
 import concurrent.futures
 import numpy as np
 from mtalg.tools.__get_num_threads import MAX_NUM_THREADS
-argmax = lambda iterable: max(enumerate(iterable), key=lambda x: x[1])[0]
 
+argmax = lambda iterable: max(enumerate(iterable), key=lambda x: x[1])[0]
 NUM_THREADS = MAX_NUM_THREADS
+
 
 class MultithreadedRNG:
 
-    def __init__(self, seed=None, threads=None):
+    def __init__(self, seed=None, num_threads=None):
 
-        self.threads = threads or max(threads, NUM_THREADS)
+        self.num_threads = num_threads or max(num_threads, NUM_THREADS)
         seq = SeedSequence(seed)
-        self._random_generators = [default_rng(s) for s in seq.spawn(threads)]
+        self._random_generators = [default_rng(s) for s in seq.spawn(num_threads)]
         self.shape = 0,
         self.shp_max = 0
         self.values = np.empty(self.shape)
         self.steps = []
-        self.executor = concurrent.futures.ThreadPoolExecutor(threads)
+        self.executor = concurrent.futures.ThreadPoolExecutor(num_threads)
 
     def standard_normal(self, size, dtype=np.float64):
         self._check_shape(size)
@@ -34,7 +35,7 @@ class MultithreadedRNG:
 
     def _fill(self, func, **kwargs):
         futures = {}
-        for i in range(self.threads):
+        for i in range(self.num_threads):
             args = (func,
                     self._random_generators[i],
                     self.values,
@@ -45,19 +46,20 @@ class MultithreadedRNG:
 
     def _check_shape(self, size):
         if size != self.shape:
+            if isinstance(size, (int, float, complex)):
+                size = size,
             self.shape = size
             self.shp_max = argmax(self.shape)
             self.values = np.empty(self.shape)
-            self.steps = [(t * (self.shape[self.shp_max] // self.threads),
-                           (t + 1) * (self.shape[self.shp_max] // self.threads))
-                          if t < (self.threads - 1) else
-                          (t * (self.shape[self.shp_max] // self.threads), self.shape[self.shp_max])
-                          for t in range(self.threads)]
+            self.steps = [(t * (self.shape[self.shp_max] // self.num_threads),
+                           (t + 1) * (self.shape[self.shp_max] // self.num_threads))
+                          if t < (self.num_threads - 1) else
+                          (t * (self.shape[self.shp_max] // self.num_threads), self.shape[self.shp_max])
+                          for t in range(self.num_threads)]
 
     def __del__(self):
         self.executor.shutdown(False)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     pass

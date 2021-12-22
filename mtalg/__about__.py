9df0d4c58c0e674dc1b878b03a305f516df4f76d
Luca Mingarelli <lucamingarelli@me.com>
2021-12-22 01:46:38 +0100

    Updated __about__

--- mtalg/__about__.py
+++ mtalg/__about__.py
@@ -2,5 +2,5 @@ __title__ = "mtalg"
 __about__ = "A library for elementwise multithreaded algebra and multithreaded random number generation."
 __version__ = '0.0.6'
 __authors__ = 'Wouter Wakker and Luca Mingarelli'
-__email__ = "wouter.wakker@outlook.com; luca.mingarelli@ecb.europa.eu"
+__email__ = "lucamingarelli@me.com"
 __url__ = "https://github.com/WWakker/mtalg"

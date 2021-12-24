d1b461127dada37edc30f8246942c4f126bdd3c9
Luca Mingarelli <lucamingarelli@me.com>
2021-12-24 02:23:05 +0100

    Create test_cdsw_reduction.py
    Added test CDSW threads limitation

---
+++ mtalg/testing/test_cdsw_reduction.py
@@ -0,0 +1,23 @@
+import os
+os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
+os.environ['DISC_KRB_REALM'] = 'MIT01.ECB.DE'
+os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
+os.environ['CDSW_CPU_MILLICORES'] = 1000
+print(os.name)
+
+
+from mtalg.random import MultithreadedRNG
+import numpy as np
+
+
+class TestMRNG:
+    def test1(self):
+        mrng = MultithreadedRNG(seed=1)
+        assert mrng.values.size == 0
+        assert mrng.num_threads == 1
+
+    def test2(self):
+        mrng = MultithreadedRNG(seed=1, num_threads=4)
+        mrng.standard_normal(4)
+        assert mrng.values.shape == (4,)
+        assert mrng.num_threads == 1

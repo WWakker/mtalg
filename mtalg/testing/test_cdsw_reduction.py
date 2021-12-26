1bca35339b9b300288a4d7acaf576eb68b6f4933
WWakker <wouter.wakker@outlook.com>
2021-12-26 13:39:13 +0100

    Fixed cdsw tests

--- mtalg/testing/test_cdsw_reduction.py
+++ mtalg/testing/test_cdsw_reduction.py
@@ -1,23 +1,29 @@
-# import os
-# os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
-# os.environ['DISC_KRB_REALM'] = 'MIT01.ECB.DE'
-# os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
-# os.environ['CDSW_CPU_MILLICORES'] = '1000'
-# print(os.name)
+import os
+from mtalg.random import MultithreadedRNG
 
 
-# from mtalg.random import MultithreadedRNG
-# import numpy as np
+def cdsw(func):
+    def wrapper():
+        os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
+        os.environ['DISC_KRB_REALM'] = 'MIT01.ECB.DE'
+        os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
+        os.environ['CDSW_CPU_MILLICORES'] = '1000'
+        func()
+        for env in ['CDSW_NODE_NAME', 'DISC_KRB_REALM', 'CDSW_NODE_NAME', 'CDSW_CPU_MILLICORES']:
+            del os.environ[env]
+    return wrapper
 
 
-# class TestMRNGCDSW:
-#     def test1(self):
-#         mrng = MultithreadedRNG(seed=1)
-#         assert mrng.values.size == 0
-#         assert mrng.num_threads == 1
+class TestMRNGCDSW:
+    @cdsw
+    def test1(self):
+        mrng = MultithreadedRNG(seed=1)
+        assert mrng.values.size == 0
+        assert mrng.num_threads == 1
 
-#     def test2(self):
-#         mrng = MultithreadedRNG(seed=1, num_threads=4)
-#         mrng.standard_normal(4)
-#         assert mrng.values.shape == (4,)
-#         assert mrng.num_threads == 1
+    @cdsw
+    def test2(self):
+        mrng = MultithreadedRNG(seed=1, num_threads=4)
+        mrng.standard_normal(4)
+        assert mrng.values.shape == (4,)
+        assert mrng.num_threads == 1

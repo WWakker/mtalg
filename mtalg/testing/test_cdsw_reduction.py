842b95eca0ac628222eb8bf38d706dfb8030b7ab
WWakker <wouter.wakker@outlook.com>
2021-12-26 13:45:10 +0100

    Fixed cdsw tests

--- mtalg/testing/test_cdsw_reduction.py
+++ mtalg/testing/test_cdsw_reduction.py
@@ -3,12 +3,12 @@ from mtalg.random import MultithreadedRNG
 
 
 def cdsw(func):
-    def wrapper():
+    def wrapper(self):
         os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
         os.environ['DISC_KRB_REALM'] = 'MIT01.ECB.DE'
         os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
         os.environ['CDSW_CPU_MILLICORES'] = '1000'
-        func()
+        func(self)
         for env in ['CDSW_NODE_NAME', 'DISC_KRB_REALM', 'CDSW_NODE_NAME', 'CDSW_CPU_MILLICORES']:
             del os.environ[env]
     return wrapper

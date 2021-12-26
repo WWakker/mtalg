3c41aaee42e44fd788d8671114add9bee8cb2f00
WWakker <wouter.wakker@outlook.com>
2021-12-26 13:48:38 +0100

    Fixed cdsw tests

--- mtalg/testing/test_cdsw_reduction.py
+++ mtalg/testing/test_cdsw_reduction.py
@@ -6,7 +6,6 @@ def cdsw(func):
     def wrapper(self):
         os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
         os.environ['DISC_KRB_REALM'] = 'MIT01.ECB.DE'
-        os.environ['CDSW_NODE_NAME'] = 'CDSW_NODE_NAME'
         os.environ['CDSW_CPU_MILLICORES'] = '1000'
         func(self)
         for env in ['CDSW_NODE_NAME', 'DISC_KRB_REALM', 'CDSW_NODE_NAME', 'CDSW_CPU_MILLICORES']:

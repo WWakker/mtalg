ae59b54d8fc3064e8ce917e5f0e3f20e719cedd3
WWakker <wouter.wakker@outlook.com>
2021-12-27 17:19:19 +0100

    Removed returning of values, method already does it

--- mtalg/random/__init__.py
+++ mtalg/random/__init__.py
@@ -3,17 +3,17 @@ from .random_number_generators import MultithreadedRNG
 
 __RNG = MultithreadedRNG()
 
-get_methods = lambda obj: [m for m in dir(obj) if callable(getattr(obj, m)) and m[0]!='_']
+get_methods = lambda obj: [m for m in dir(obj) if callable(getattr(obj, m)) and m[0] != '_']
 
 for method in get_methods(__RNG):
     argsspec = inspect.getfullargspec(getattr(__RNG, method))
     args_all, defaults = argsspec.args[1:], argsspec.defaults or []
-    kwargs = {k:v for k,v in zip(args_all[len(args_all)-len(defaults):], defaults)}
-    args = args_all[:len(args_all)-len(defaults)]
+    kwargs = {k: v for k, v in zip(args_all[len(args_all) - len(defaults):], defaults)}
+    args = args_all[:len(args_all) - len(defaults)]
+
 
     def func(*args, **kwargs):
         getattr(__RNG, method)(*args, **kwargs)
-        return __RNG._values
+
 
     exec(f'{method}=func')
-

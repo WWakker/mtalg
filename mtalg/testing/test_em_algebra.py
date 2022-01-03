b27b2a374ef599ad94320757f0a46a45091c6aba
Luca Mingarelli <lucamingarelli@me.com>
2022-01-03 16:31:11 +0100

    Update test_em_algebra.py

--- mtalg/testing/test_em_algebra.py
+++ mtalg/testing/test_em_algebra.py
@@ -30,6 +30,16 @@ def get_a_b(size, no_negative=False):
 
 
 class TestEmAlgebra:
+
+    def test_missing_numba(self):
+        os.system("pip uninstall --yes numba")
+        import mtalg
+        
+        a = np.arange(100)
+        with pytest.raises(ModuleNotFoundError):
+            mtalg.std(a)
+        os.system("pip install numba")
+        
     def test_all(self):
         import mtalg
         failed = {}
@@ -66,15 +76,8 @@ class TestEmAlgebra:
         a = np.arange(100)
         assert np.isclose(np.std(a), mtalg.std(a))
 
-    def test4(self):
-        os.system("pip uninstall --yes numba")
-        
-        import mtalg
-        a = np.arange(100)
-        with pytest.raises(ModuleNotFoundError):
-            mtalg.std(a)
 
-    def test5(self):
+    def test4(self):
         import mtalg
         a_arr = np.arange(10000).reshape((100, 100))
         a_scal = 2

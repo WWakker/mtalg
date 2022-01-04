18870ca96ffe2e5c0f5854d8c1f07d91671fca16
WWakker <wouter.wakker@outlook.com>
2022-01-04 15:11:21 +0100

    Fixed test

--- mtalg/testing/test_core.py
+++ mtalg/testing/test_core.py
@@ -2,6 +2,7 @@ import mtalg
 import pytest
 
 import mtalg.core.threads
+from multiprocessing import cpu_count
 
 
 class TestCore:
@@ -14,6 +15,6 @@ class TestCore:
             mtalg.set_num_threads(1.5)
 
     def test3(self):
-        assert mtalg.core.threads._global_num_threads is None
+        assert mtalg.core.threads._global_num_threads == cpu_count()
         mtalg.set_num_threads(4)
         assert mtalg.core.threads._global_num_threads == 4

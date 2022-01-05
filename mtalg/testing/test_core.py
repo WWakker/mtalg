84f45df69d2fed5147d010bbf2448660a3a1b2cf
Luca Mingarelli <lucamingarelli@me.com>
2022-01-05 19:42:51 +0100

    Fixed testing in test_core.py

--- mtalg/testing/test_core.py
+++ mtalg/testing/test_core.py
@@ -14,8 +14,8 @@ class TestCore:
         with pytest.raises(ValueError):
             mtalg.set_num_threads(1.5)
 
-    def test3(self):
+    def test3(self, n_threads=4):
         assert mtalg.core.threads._global_num_threads == cpu_count()
-        mtalg.set_num_threads(4)
-        mtalg.get_num_threads()
-        assert mtalg.core.threads._global_num_threads == 4
+        mtalg.set_num_threads(n_threads)
+        assert mtalg.core.threads._global_num_threads == n_threads
+        assert mtalg.get_num_threads() == n_threads

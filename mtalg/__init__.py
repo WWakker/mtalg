31d7911af39fa4d4c851842642570316b11bf587
Luca Mingarelli <lucamingarelli@me.com>
2022-01-05 17:21:29 +0100

    Update __init__.py
    importing get_num_threads

--- mtalg/__init__.py
+++ mtalg/__init__.py
@@ -4,4 +4,5 @@ from mtalg.__about__ import (__title__, __version__,
 from mtalg.alg import add, sub, div, mul, pow, std
 import mtalg.random
 from mtalg.tools.__set_threads import set_num_threads
+from mtalg.tools.__get_threads import get_num_threads
 import mtalg.core.threads
